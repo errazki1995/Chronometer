9402f9ec55b974f4f0291723b349ba2d89d050f0
ayoub <ayouberrazki@outlook.com>
2019-10-11 17:47:43 +0100

    Code Ui Ok| Handler Ko

--- app/src/main/java/com/errazki/chronometer/MainActivity.java
+++ app/src/main/java/com/errazki/chronometer/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private Thread timerThread;
     private Button stopbtn;
     private Button startbtn;
+    private Button initbtn;
+    private Runnable refreshRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +24,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainText = (TextView) findViewById(R.id.mainTextView);
         stopbtn = (Button) findViewById(R.id.stopbtn);
-       startbtn = (Button) findViewById(R.id.startbtn);
-        handler = new Handler();
+        startbtn = (Button) findViewById(R.id.startbtn);
+        initbtn = (Button) findViewById(R.id.initbtn);
+        this.handler = new Handler();
         state = new State();
+        //runnableInitUi();
+        runnableInitHandler();
     }
 
+    public Runnable getRefreshRunnable() {
+        return refreshRunnable;
+    }
+
+    public void setRefreshRunnable(Runnable refreshRunnable) {
+        this.refreshRunnable = refreshRunnable;
+    }
 
     static class State {
         long cumulatedTime = 0; // cumulated time from the previous runs
         long startTime = -1; // startTime for the current run
-        String readabletime;
+        String readabletime = "00:00:00:00";
 
         public long getElapsedTime1() {
             if (startTime > 0) {
@@ -53,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        public String getreadabletime() {
+        public String getReadabletime() {
             return readabletime;
         }
 
@@ -79,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private String getReadableTime(Long nanos) {
+    private String getReadableTimeMain(Long nanos) {
         long nanotemp = nanos;
         long tempSec = nanos / (1000 * 1000 * 1000);
         long sec = tempSec % 60;
@@ -87,29 +99,27 @@ public class MainActivity extends AppCompatActivity {
         long hour = (tempSec / (60 * 60)) % 24;
         long day = (tempSec / (24 * 60 * 60)) % 24;
         nanotemp = nanotemp - tempSec;
-        String numberStr="";
-        if(nanotemp>0){
-             numberStr = String.valueOf(nanotemp).substring(0, 3);
+        String numberStr = "0";
+        if (nanotemp > 0) {
+            numberStr = String.valueOf(nanotemp).substring(0, 3);
 
         }
         return String.format("%d:%d:%d", hour, min, sec) + ":" + numberStr;
-}
+    }
 
     public void startChrono() {
-        /*au cas ou le temps de depart est -1(le debut) on met le temps recent
-         *sinon on met comme temps de depart le (temps d'arret précédent pour continuer)
-         */
+
         startbtn.setEnabled(false);
         stopbtn.setEnabled(true);
         state.setStartTime(System.nanoTime());
-        //updateChronoOnHandlerMethod();
-        updateChronoOnUiThreadMethod();
+        updateChronoOnHandlerMethod();
+        //updateChronoOnUiThreadMethod();
     }
 
 
     public void stopChrono() {
-       startbtn.setEnabled(true);
-       if (stopbtn.isEnabled()) {
+        startbtn.setEnabled(true);
+        if (stopbtn.isEnabled()) {
             state.setCumulatedTime(state.getElapsedTime());
             state.setStartTime(-1);
             if (timerThread != null) timerThread.interrupt();
@@ -118,9 +128,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View v) {
-        //startbtn.setEnabled(false);
-        startChrono();
 
+        startChrono();
     }
 
     public void stop(View v) {
@@ -133,61 +142,49 @@ public class MainActivity extends AppCompatActivity {
         mainText.setText("00:00:00:00");
     }
 
+    public void refreshChrono() {
+        String chrono = timeformat(state.getElapsedTime());
+        Log.w("chrono", chrono + "the time in nano: " + state.getElapsedTime());
+        state.setReadabletime(chrono);
+        mainText.setText(state.getReadabletime());
+    }
+
+
+
     public String timeformat(long nano) {
-        return getReadableTime(nano);
+        return getReadableTimeMain(nano);
     }
 
-    /*
-     *
-     *Ce bout du code utilise le Handler , cette pratique est souvent utilisé
-     */
-    public void updateChronoOnHandlerMethod() {
-        timerThread = new Thread(() -> {
-            while (!Thread.interrupted()) {
 
-                // le code Ui doit etre executé soit dans le runOnUIThread ou le handler
-                handler.post(() -> {
-                    String chrono = timeformat(state.getElapsedTime());
+    /*******************************************************Handler Thread Method****************************************/
 
-                    Log.w("chrono", chrono + "the time in nano: " + state.getElapsedTime());
-
-                    state.setReadabletime(chrono);
-                    mainText.setText(state.getreadabletime());
-                    try {
-                        Thread.sleep(500);
-
-                    } catch (InterruptedException e) {
-
-                    }
-
-                });
-            }
-
-
-        });
-        timerThread.start();
-
+    public void startRefresh(){
+        handler.post(getRefreshRunnable());
+    }
+    public void stopRefresh(){
+        handler.removeCallbacks(getRefreshRunnable());
+    }
+    public void runnableInitHandler(){
+        refreshRunnable=()->{
+            refreshChrono();
+        };
+      setRefreshRunnable(refreshRunnable);
     }
 
-    /*
-     *Ce bout du code utilise le OnUiThread
-     */
-    public void updateChronoOnUiThreadMethod() {
-
-        timerThread = new Thread() {
+ public void updateChronoOnHandlerMethod() {
+        handler.postDelayed(getRefreshRunnable(),500);
+ }
+/**************************************************************OnUiThreadMethod******************************************/
+    public void runnableInitUi() {
+        refreshRunnable = new Runnable() {
             @Override
             public void run() {
                 try {
-                    while (!isInterrupted()) {
+                    while (!Thread.interrupted()) {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                String chrono = timeformat(state.getElapsedTime());
-                                Log.w("chronoo", chrono + "the time in nano: " + state.getElapsedTime());
-
-                                state.setReadabletime(chrono);
-                                mainText.setText(state.getreadabletime());
-
+                                refreshChrono();
                             }
                         });
                         try {
@@ -201,6 +198,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+        setRefreshRunnable(refreshRunnable);
+
+    }
+    public void updateChronoOnUiThreadMethod() {
+        timerThread = new Thread(getRefreshRunnable());
         timerThread.start();
 
     }
